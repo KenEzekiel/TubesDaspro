668af8e63c8db54f7ae4d334d54471d53b3e3625
KENNETH EZEKIEL <16521040@mahasiswa.itb.ac.id>
2022-04-01 20:11:12 +0700

    Create magicconch.py
    Bonus Module for tubes

---
+++ Bonus-Modules/magicconch.py
@@ -0,0 +1,29 @@
+import time
+
+def magicconch():
+    x = time.time()
+    a = 3
+    c = 1
+    m = 7
+    state = round(((a*x) + c) % m)
+    if state == 0:
+        return "Coba lagi."
+    elif state == 1:
+        return "Ya."
+    elif state == 2:
+        return "Tidak."
+    elif state == 3:
+        return "Mungkin."
+    elif state == 4:
+        return "Jangan deh."
+    elif state == 5:
+        return "Tanya lagi nanti."
+    elif state == 6:
+        return "Terserah dah."
+    elif state == 7:
+        return "Coba tanya doswal."
+
+
+for i in range(10):
+    time.sleep(0.3)
+    print(magicconch())
