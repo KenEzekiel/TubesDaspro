7c873f23141975ab69ea6a59f50f415314992c9b
KENNETH EZEKIEL <16521040@mahasiswa.itb.ac.id>
2022-04-01 18:46:30 +0700

    Fixed reader-writer.py to use the standard library

--- reader-writer.py
+++ reader-writer.py
@@ -1,18 +1,20 @@
+from standard import *
+
 def reader(filename):
     with open(f"Database/{filename}", "r") as f:
         result = []
         for line in f:
-            line = line.rstrip()  # strip '\n'
-            print(line)
+            line = line.rstrip()                        # strip '\n'
+            #print(line)
             line_objects = []
             word_holder = ""
-            for i in range(len(line)):
+            for i in range(length(line)):
                 if line[i] == ";":
                     line_objects.append(word_holder)
                     word_holder = ""
                 else:
                     word_holder += line[i]
-                if i == len(line)-1:
+                if i == length(line)-1:
                     line_objects.append(word_holder)
                     word_holder = ""
             result.append(line_objects)
@@ -27,25 +29,21 @@ def reader(filename):
 def writeline(filename, array):
     with open(f"Database/{filename}", "w") as f:
         line = ""
-        for i in range(len(array)):
-            for j in range(len(array[i])):
-                if j == len(array[i]) - 1:
+        for i in range(length(array)):             # Looping for every line of data
+            for j in range(length(array[i])):      # Looping for every item in line
+                if j == length(array[i]) - 1:      # If item is the last item, doesn't use ;
                     line += str(array[i][j])
                 else:
-                    line += f"{array[i][j]};"
-            line += "\n"
+                    line += f"{array[i][j]};"   # use ;
+            line += "\n"                        # end of line
         f.write(line)
 
 def writer(filename, data_add):
     data = reader(filename)
-    #add_data = ['1', 'oscta', 'action', '1990', '17000', '6']
     data.append(data_add)
-
     writeline(filename, data)
 
-    print(data)
-#print(add_data)
-#print(final_data)
-#print(reader("game.csv"))
+    #print(data)
 
-writer("game.csv", ['2', 'hai', 'action', '1990', '17000', '6'])
+#add_data = ['1', 'oscta', 'action', '1990', '17000', '6']
+writer("game.csv", ['3', 'octafx', 'action', '1990', '17000', '6'])
