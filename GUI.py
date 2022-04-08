29932552cd84cd97204bdd35d87993e66686c856
KENNETH EZEKIEL <16521040@mahasiswa.itb.ac.id>
2022-04-08 23:14:57 +0700

    Imported and laid the foundation for Tkinter GUI

--- GUI.py
+++ GUI.py
@@ -1,7 +1,21 @@
-import loader
+import tkinter
+#import loader
 import readerwriter
 
-running = False
+#--------------- TKINTER ---------------------
+root = tkinter.Tk()
+root.title("PROGRAM")
+#root.iconbitmap("resources/logo_itb.ico")
+
+font_tuple = ("Colibri", 10)
+
+input_command = tkinter.Entry(root, text="Command", font=font_tuple, bg="#B6D6EB")
+input_command.grid(row=1, column=1)
+output_field = tkinter.Label(root, text="test", font=font_tuple, bg="#B6D6EB")
+output_field.grid(row=2, column=1)
+#---------------------------------------------
+
+"""running = False
 if loader.save_folder in loader.all_folder:
     print("Loading...")
     print('Welcome to the "Binomo" Interface')
@@ -14,4 +28,6 @@ while running:
     filenames = ["game.csv", "kepemilikan.csv", "riwayat.csv", "user.csv"]
     data = [readerwriter.reader(loader.save_folder, file) for file in filenames]
     print(data)
-    running = False
+    running = False"""
+
+root.mainloop()
