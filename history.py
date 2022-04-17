b33e8d79bd898b4ec408962299d068bf02c3fe2d
melvinkent <16521247@mahasiswa.itb.ac.id>
2022-04-17 18:24:56 +0700

    Update history.py
    All the algorithm is changed to be able to generate proper parsing

--- history.py
+++ history.py
@@ -1,39 +1,32 @@
-import readerwriter
 import standard
 
-
-def history(save_folder: str):
-    """
-    Procedure to print the content of a riwayat.csv from a save folder
-    """
-
-    data_history = readerwriter.reader(save_folder, "riwayat.csv")
-    length_data = standard.length(data_history)
-
-    if length_data == 1:
-        print("Sorry, you haven't bought any game yet. Enter buy_game to buy some game")
-    else:
-        print("NO | ID | NAMA | HARGA | USER ID | TAHUN BELI")
-        for i in range(1, length_data):
-            print(i, end=". ")
-            for j in range(standard.length(data_history[i])):
-                print(data_history[i][j], end=" | ")
-            print("")
-
-
-def historyFromMatrix(hist_data: list):
+def history(hist_data: list):
     """
     Procedure to print the content of riwayat.csv array from the temporary data matrix
     """
 
-    length_data = standard.length(hist_data)
+    if standard.length(hist_data) == 1 :
+        print("Sorry, you haven't bougth any game yet. Enter buy_game to bur some game.")    
+    
+    else :
+        # generate a temporary list for hosting data without changing the source
+        data_history = ["*" for i in range(standard.length(hist_data)-1)]
+        for i in range (1, standard.length(hist_data)) :                # traversing from 1 to skip data heading
+            data_history[i-1] = hist_data[i]
 
-    if length_data == 1:
-        print("Sorry, you haven't bought any game yet. Enter buy_game to buy some game")
-    else:
-        print("NO | ID | NAMA | HARGA | USER ID | TAHUN BELI")
-        for i in range(1, length_data):
-            print(i, end=". ")
-            for j in range(standard.length(hist_data[i])):
-                print(hist_data[i][j], end=" | ")
+        data_history = standard.append(["NUM", "ID", "NAME", "PRICE", "USER ID", "PURCHASE YEAR"], data_history)
+
+        # Generate parsing for non-empty data_history list
+        for i in range (standard.length(data_history)): 
+            print(str(i+1) + ".", end=" ")
+
+            for j in range (6) :
+                print(data_history[i][j], end="")
+                character_amount = 0
+
+                for k in range (standard.length(data_history)):
+                    if standard.length(str(data_history[k][j])) > character_amount :
+                        character_amount = standard.length(str(data_history[k][j]))
+                
+                print((" " * (character_amount- standard.length(str(data_history[i][j])))), "| ", end="")
             print("")
