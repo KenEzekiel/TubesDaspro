6abe38c7ac3d8a60bf3ea11f20429a85a3fabf75
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2022-04-23 18:53:55 +0700

    Removed unnecessary comment and made the comment on top of the file clearer

--- standard.py
+++ standard.py
@@ -1,4 +1,5 @@
-# Standard function library
+# Standard function library (manual implementation of python functions that is not allowed to be used)
+
 
 def length(input) -> int:
     """
@@ -49,22 +50,9 @@ def enum(list_: list, start=0):
     """
     Works the same way as the built-in function enumerate(). Returns an enumerate object. 
     """
+    
     n = start
 
     for elem in list_:
         yield n, elem
         n += 1
-
-
-# def join_list(string: str, iterable) -> str:
-#     """
-#     Returns a string by joining all the elements of an iterable (list, string, tuple), separated by a string separator.
-#     Made as an alternative of the string method str.join().
-#     """
-
-#     output = ''
-
-#     for i in iterable:
-#         output += f"{i}{string}"
-
-#     return output
